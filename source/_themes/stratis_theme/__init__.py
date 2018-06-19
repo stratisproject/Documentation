5a550e66235be8f9656b8e4419905fe477dfd0c8
Ben Auld <ben@popcornwebdesign.co.uk>
2018-06-19 16:05:36 +0100

    Starts Stratis theme for Sphinx

---
+++ source/_themes/stratis_theme/__init__.py
@@ -0,0 +1,16 @@
+"""Sphinx Stratis theme.
+"""
+from os import path
+
+__version__ = '0.1.0'
+__version_full__ = __version__
+
+
+def get_html_theme_path():
+    """Return list of HTML theme paths."""
+    cur_dir = path.abspath(path.dirname(path.dirname(__file__)))
+    return cur_dir
+
+# See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
+def setup(app):
+    app.add_html_theme('stratis_theme', path.abspath(path.dirname(__file__)))
